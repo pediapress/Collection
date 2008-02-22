77cfb899b5c087b4ff033c194a3c8013b007ab84
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-22 13:19:28 +0000

    article -> page

--- Collection.i18n.php
+++ Collection.i18n.php
@@ -24,31 +24,31 @@
 $messages = array();
 
 $messages['en'] = array(
-	'coll-desc' => '[[Special:Collection|Collect articles]], generate PDFs',
+	'coll-desc' => '[[Special:Collection|Collect pages]], generate PDFs',
 	'coll-collection' => 'Collection',
 	'coll-collections' => 'Collections',
 	'coll-portlet_title' => 'My Collection',
-	'coll-add_article' => 'Add article',
-	'coll-remove_article' => 'Remove article',
+	'coll-add_page' => 'Add page',
+	'coll-remove_page' => 'Remove page',
 	'coll-add_category' => 'Add category',
 	'coll-load_collection' => 'Load collection',
 	'coll-show_collection' => 'Show collection',
 	'coll-help_collections' => 'Collections help',
 	'coll-empty' => 'empty',
-	'coll-article' => 'article',
-	'coll-articles' => 'articles',
+	'coll-page' => 'page',
+	'coll-pages' => 'pages',
 	'coll-download_as_pdf' => 'Download as PDF',
 	'coll-noscript_text' => '<noscript><h1>JavaScript is Required!</h1>
 <strong>Your browser does not support JavaScript or JavaScript has been turned off.
 This page will not work correctly, unless JavaScript is enabled.</strong></noscript>',
-	'coll-intro_text' => "You can collect articles, generate and download a PDF file from article collections and save article collections for later use or to share them.
+	'coll-intro_text' => "You can collect pages, generate and download a PDF file from page collections and save page collections for later use or to share them.
 
 See [[Help:Collections]] for more information about collections.",
-	'coll-pdftoobigcat' => 'The category contains more than %PARAM% articles, only the first %PARAM% articles can be added to your collection.
+	'coll-pdftoobigcat' => 'The category contains more than %PARAM% pages, only the first %PARAM% pages can be added to your collection.
 Do you want to add them?',
 	'coll-my_collection' => 'My Collection',
 	'coll-download_title' => 'Download Collection as PDF',
-	'coll-download_text' => 'To download an automatically generated PDF file of your article collection, click the button.',
+	'coll-download_text' => 'To download an automatically generated PDF file of your page collection, click the button.',
 	'coll-download_pdf' => 'Download PDF',
 	'coll-remove' => 'Remove',
 	'coll-move_to_top' => 'Move to top',
@@ -59,7 +59,7 @@ Do you want to add them?',
 	'coll-subtitle' => 'Subtitle:',
 	'coll-contents' => 'Contents',
 	'coll-create_chapter' => 'Create new chapter',
-	'coll-sort_alphabetically' => 'Sort articles alphabetically',
+	'coll-sort_alphabetically' => 'Sort pages alphabetically',
 	'coll-clear_collection' => 'Clear collection',
 	'coll-clear_confirm' => 'Do you really want to clear your collection?',
 	'coll-rename' => 'Rename',
@@ -68,10 +68,10 @@ Do you want to add them?',
 	'coll-no_such_category' => 'No such category',
 	'coll-mwpdf_error_title' => 'Could not generate PDF',
 	'coll-mwpdf_error_msg' => 'An error occured during PDF generation.',
-	'coll-mwzip_error_title' => 'Could not generate article collection',
-	'coll-mwzip_error_msg' => 'An error occured during generation of article collection.',
-	'coll-notitle_title' => 'Could not get article title',
-	'coll-notitle_title' => 'The title of the article could not be determined.',
+	'coll-mwzip_error_title' => 'Could not generate page collection',
+	'coll-mwzip_error_msg' => 'An error occured during generation of page collection.',
+	'coll-notitle_title' => 'Could not get page title',
+	'coll-notitle_title' => 'The title of the page could not be determined.',
 	'coll-post_failed_title' => 'POST request failed',
 	'coll-post_failed_msg' => 'The POST request failed.',
 	'coll-enter_title' => 'Enter title for collection:',
@@ -89,14 +89,14 @@ Do you want to add them?',
 Do you want it to be replaced with your collection?',
 	'coll-yes' => 'Yes',
 	'coll-no' => 'No',
-	'coll-load_overwrite_text' => 'You already have some articles in your collection.
+	'coll-load_overwrite_text' => 'You already have some pages in your collection.
 Do want to overwrite your current collection, append the new content or cancel?',
 	'coll-overwrite' => 'Overwrite',
 	'coll-append' => 'Append',
 	'coll-cancel' => 'Cancel',
 	'coll-limit_exceeded_title' => 'Collection Too Big',
-	'coll-limit_exceeded_text' => 'Your article collection is too big.
-No more articles can be added.',
+	'coll-limit_exceeded_text' => 'Your page collection is too big.
+No more pages can be added.',
 	'coll-generating_pdf_title' => 'Generating PDF',
 	'coll-generating_pdf_text' => "'''Please wait while the PDF file is being generated.'''
 This page should automatically refresh every few seconds, if this does not work, please press refresh button of your browser.",
@@ -108,5 +108,5 @@ Click on the link below to download it to your computer.",
 	'coll-notfound_title' => 'Collection Not Found',
 	'coll-notfound_text' => 'Could not find collection page.',
 	'coll-return_to_collection' => '<p>Return to <a href="$referrer_link">$referrer_name</a></p>',
-	'coll-articles_removed' => 'The following articles could not be rendered and were removed from the PDF file:',
+	'coll-pages_removed' => 'The following pages could not be rendered and were removed from the PDF file:',
 );
