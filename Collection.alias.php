5f0b500a0325db6bd50d2335b740134e52e4a994
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-08 09:37:07 +0000

    Localisation updates for extension special page aliases from Betawiki

--- Collection.alias.php
+++ Collection.alias.php
@@ -12,3 +12,24 @@ $aliases = array();
 $aliases['en'] = array(
 	'Collection' => array( 'Collection' ),
 );
+
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'Collection' => array( 'مجموعة' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'Collection' => array( 'مجموعة' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'Collection' => array( 'Verzameling', 'Collectie' ),
+);
+
+/** Swedish (Svenska) */
+$aliases['sv'] = array(
+	'Collection' => array( 'Samling' ),
+);
+
