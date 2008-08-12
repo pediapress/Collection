537e3fe4e0dc235ca2aafc19aaad97229023b912
Johannes Beigel <jojo@users.mediawiki.org>
2008-08-12 15:00:21 +0000

    make portlet for logged-in only users configurable

--- Collection.php
+++ Collection.php
@@ -70,6 +70,8 @@ $wgCollectionFormats = array(
 	'rl' => 'PDF',
 );
 
+$wgCollectionPortletForLoggedInUsersOnly = false;
+
 # ==============================================================================
 
 
