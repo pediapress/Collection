1dade92c5f104a673c6a2630b0b8d9c07cd7c643
jojo <jojo@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-01-28 15:14:02 +0000

    replaced Categoryfinder usage with an internal call to MediaWiki API
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Collection@46447 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Collection.body.php
+++ Collection.body.php
@@ -935,20 +935,30 @@ class Collection extends SpecialPage {
 		$wgOut->addTemplate( $template );
 	}
 
-	static function isCollectionPage( $title, $article ) {
+	static function isCollectionPage( $article ) {
 		wfLoadExtensionMessages( 'Collection' );
 
-		if ( is_null( $title ) || is_null( $article ) ) {
+		if ( is_null( $article ) ) {
 			return false;
 		}
 
-		$categoryFinder = new Categoryfinder();
-		$categoryFinder->seed( array( $article->getID() ), array( wfMsgForContent( 'coll-bookscategory' ) ) );
-		$articles = $categoryFinder->run();
-		if ( in_array( $article->getID(), $articles ) ) {
-			return true;
-		}
-		return false;
+    $params = new FauxRequest(array(
+      'action' => 'query',
+      'pageids' => $article->getID(),
+      'prop' => 'categories',
+    ));
+    $api = new ApiMain($params);
+    $api->execute();
+    $result = $api->getResultData();
+    $catTitle = Title::makeTitle(NS_CATEGORY, wfMsgForContent('coll-bookscategory'));
+    if (isset($result['query']['pages'][$article->getID()]['categories'])) {
+      foreach ($result['query']['pages'][$article->getID()]['categories'] as $cat) {
+        if ($cat['title'] == $catTitle) {
+          return true;
+        }
+      }
+    }
+    return false;
 	}
 
 	/**
@@ -964,7 +974,7 @@ class Collection extends SpecialPage {
 		$action = $wgRequest->getVal('action');
 
 		if ( $skinTemplate->iscontent && ( $action == '' || $action == 'view' || $action == 'purge' ) ) {
-			if ( self::isCollectionPage( $skinTemplate->mTitle, $wgArticle ) ) {
+			if ( self::isCollectionPage( $wgArticle ) ) {
 				$params = 'colltitle=' . wfUrlencode( $skinTemplate->mTitle->getPrefixedDBKey() );
 				if ( isset( $wgCollectionFormats['rl'] ) ) {
 					$nav_urls['printable_version_pdf'] = array(
@@ -1094,7 +1104,7 @@ EOS
 		
 		$out = "<ul id=\"collectionPortletList\">";
 		
-		if ( self::isCollectionPage( $wgTitle, $wgArticle) ) {
+		if ( self::isCollectionPage( $wgArticle) ) {
 			$params = "colltitle=" . $wgTitle->getPrefixedUrl();
 			$href = htmlspecialchars( SkinTemplate::makeSpecialUrlSubpage(
 				'Book',
