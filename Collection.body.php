f89a8e75a64a0ac8de6c15ff1d30e7bee252b745
jojo <jojo@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-02-23 11:36:47 +0000

    use a pattern instead, is the more general solution.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Collection@47702 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Collection.body.php
+++ Collection.body.php
@@ -717,7 +717,7 @@ class Collection extends SpecialPage {
 			'template_blacklist' => wfMsgForContent( 'coll-template_blacklist_title' ),
 			'template_exclusion_category' => wfMsgForContent( 'coll-exclusion_category_title' ),
 			'print_template_prefix' => wfMsgForContent( 'coll-print_template_prefix' ),
-			'print_template_subpage' => wfMsgForContent( 'coll-print_template_subbpage' ),
+			'print_template_pattern' => wfMsgForContent( 'coll-print_template_pattern' ),
 			'language' => $wgContLang->getCode(),
 			'writer' => $writer,
 		) );
@@ -754,7 +754,7 @@ class Collection extends SpecialPage {
 			'template_blacklist' => wfMsgForContent( 'coll-template_blacklist_title' ),
 			'template_exclusion_category' => wfMsgForContent( 'coll-exclusion_category_title' ),
 			'print_template_prefix' => wfMsgForContent( 'coll-print_template_prefix' ),
-			'print_template_subpage' => wfMsgForContent( 'coll-print_template_subpage' ),
+            'print_template_pattern' => wfMsgForContent( 'coll-print_template_pattern' ),
 			'language' => $wgContLang->getCode(),
 			'writer' => $writer,
 			'force_render' => true
@@ -910,7 +910,7 @@ class Collection extends SpecialPage {
 			'template_blacklist' => wfMsgForContent( 'coll-template_blacklist_title' ),
 			'template_exclusion_category' => wfMsgForContent( 'coll-exclusion_category_title' ),
 			'print_template_prefix' => wfMsgForContent( 'coll-print_template_prefix' ),
-			'print_template_subpage' => wfMsgForContent( 'coll-print_template_subpage' ),
+			'print_template_pattern' => wfMsgForContent( 'coll-print_template_pattern' ),
 			'pod_api_url' => $this->mPODPartners[$partner]['posturl'],
 		) );
 		if ( !$response ) {
