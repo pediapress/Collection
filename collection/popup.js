781dc86646efba86f2e33cbfeb29d14888cd1fe0
jojo <jojo@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-03 13:31:42 +0000

    properly escape JS code using htmlspecialchars() (in PHP code) and escapeQuotesHTML() (in JS code)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Collection@51400 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- collection/popup.js
+++ collection/popup.js
@@ -12,17 +12,17 @@ var collectionPopup;
 
 // map pg.ns.index values to namespace numbers
 var popup_gadget_ns_repair = {
-    0: 0, 1: -1, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7, 9: 8, 10: 9,
-    11: 10, 12: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 100, 18: 101
+		0: 0, 1: -1, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7, 9: 8, 10: 9,
+		11: 10, 12: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 100, 18: 101
 };
 
 // replacement for Navpopup.prototype.unhide()
 function collectionPopupUnhide() {
 	this.runHooks('unhide', 'before');
 	if (typeof this.visible != 'undefined' && !this.visible && !isBadLinkForCollection()) {
-        this.mainDiv.innerHTML = getCollectionPopupHTML(this.mainDiv);
-        this.mainDiv.style.display = 'inline';
-        this.visible = true;
+		this.mainDiv.innerHTML = getCollectionPopupHTML(this.mainDiv);
+		this.mainDiv.style.display = 'inline';
+		this.visible = true;
 	}
 	this.runHooks('unhide', 'after');
 }
@@ -37,24 +37,27 @@ function collectionPopupSetInnerHTML () {
 // set the content of the popup
 function getCollectionPopupHTML(popup) {
 	collectionPopup = popup;
-    var title = pg.current.article.value;
-    var ns = getNamespaceNumber(pg.current.article.namespace());
-    var stripped_title = title;
-    if (ns != 0) {
-        stripped_title = pg.current.article.stripNamespace();
-    }
-    stripped_title = stripped_title.replace(/'/, "\\'");
+	var title = pg.current.article.value;
+	var ns = getNamespaceNumber(pg.current.article.namespace());
+	var stripped_title = title;
+	if (ns != 0) {
+		stripped_title = pg.current.article.stripNamespace();
+	}
+	stripped_title = stripped_title.replace(/"/, '\\"');
 	var popupContent = '<a onclick="';
 	if (isInCollection(title)) {
-		popupContent += 'popupCollectionCall(\'RemoveArticle\', [' + ns + ', \'' + stripped_title + '\', 0]); return false;';
+		var js = 'popupCollectionCall("RemoveArticle", [' + ns + ', "' + stripped_title + '", 0]); return false;';
+		popupContent += escapeQuotesHTML(js);
 		popupContent += '" href="#">' + wgCollectionRemovePageText + '</a>';
 	} else {
 		if (ns != 14) {
-			popupContent += 'popupCollectionCall(\'AddArticle\', [' + ns + ', \'' + stripped_title + '\', 0]); return false;';
-            popupContent += '" href="#">' + wgCollectionAddPageText + '</a>';
+			var js = 'popupCollectionCall("AddArticle", [' + ns + ', "' + stripped_title + '", 0]); return false;'
+			popupContent += escapeQuotesHTML(js);
+			popupContent += '" href="#">' + wgCollectionAddPageText + '</a>'
 		} else {
-			popupContent += 'popupCollectionCall(\'AddCategory\', [\'' + stripped_title + '\']); return false;';
-            popupContent += '" href="#">' + wgCollectionAddCategoryText + '</a>';
+			var js = 'popupCollectionCall("AddCategory", ["' + stripped_title + '"]); return false;'
+			popupContent += escapeQuotesHTML(js);
+			popupContent += '" href="#">' + wgCollectionAddCategoryText + '</a>';
 		}
 	}
 	popupContent += ' <a href="javascript:alert(wgCollectionPopupHelpText);">[?]</a>';
@@ -65,34 +68,34 @@ function getCollectionPopupHTML(popup) {
 function refreshCollectionArticleList() {
 	sajax_request_type = 'POST';
 	sajax_do_call('wfAjaxGetCollection', [], function(xhr) {
-        var articles = [];
-        function add_articles(items) {
-            for (var i = 0; i < items.length; i++) {
-                var item = items[i];
-                if (item.type == 'article') {
-                    articles.push(item.title);
-                } else if (item.type == 'chapter') {
-                    add_articles(item.items);
-                }
-            }
-        }
-        coll = JSON.parse(xhr.responseText);
-				if (typeof coll.collection.items != 'undefined' ) {
-					add_articles(coll.collection.items);
+		var articles = [];
+		function add_articles(items) {
+			for (var i = 0; i < items.length; i++) {
+				var item = items[i];
+				if (item.type == 'article') {
+					articles.push(item.title);
+				} else if (item.type == 'chapter') {
+					add_articles(item.items);
 				}
-        if (articles.length > 0) {
-            startCollectionPopups();
-        } else {
-            stopCollectionPopups();
-        }
-        collectionArticleList = articles;
+			}
+		}
+		coll = JSON.parse(xhr.responseText);
+		if (typeof coll.collection.items != 'undefined' ) {
+			add_articles(coll.collection.items);
+		}
+		if (articles.length > 0) {
+			startCollectionPopups();
+		} else {
+			stopCollectionPopups();
+		}
+		collectionArticleList = articles;
 	});
 }
 window.refreshCollectionArticleList = refreshCollectionArticleList;
 
 // hide popup, execute func via XHR, refresh article list
 function popupCollectionCall(func, args) {
-    hideCollectionPopup();
+	hideCollectionPopup();
 	sajax_request_type = 'POST';
 	sajax_do_call('wfAjaxCollection' + func, args, function(xhr) {
 		refreshCollectionArticleList();
@@ -112,61 +115,61 @@ function hideCollectionPopup(popup) {
 
 // stop popups
 function stopCollectionPopups() {
- 	if (createBookMode != false) {
- 	 	var bodyDiv = document.getElementById("bodyContent");
-	 	var links = bodyDiv.getElementsByTagName('a');
-	 	for (var i = 0; i < links.length; i++) {
-	 	 	removeTooltip(links[i]);
-	 	 	links[i].onmousedown = null;
-	 	}
-	 	createBookMode = false;
- 	}
+	if (createBookMode != false) {
+		var bodyDiv = document.getElementById("bodyContent");
+		var links = bodyDiv.getElementsByTagName('a');
+		for (var i = 0; i < links.length; i++) {
+			removeTooltip(links[i]);
+			links[i].onmousedown = null;
+		}
+		createBookMode = false;
+	}
 }
 
 // start popups
 function startCollectionPopups() { 
- 	if (createBookMode != true) {
-	 	var bodyDiv = document.getElementById("bodyContent");
-	 	var links = bodyDiv.getElementsByTagName('a');
-	 	for (var i = 0; i < links.length; i++) {
-            addTooltip(links[i]);
-	 	}
-	 	createBookMode = true;
-    }
+	if (createBookMode != true) {
+		var bodyDiv = document.getElementById("bodyContent");
+		var links = bodyDiv.getElementsByTagName('a');
+		for (var i = 0; i < links.length; i++) {
+						addTooltip(links[i]);
+		}
+		createBookMode = true;
+		}
 }
 
 // check if an article is in the collection
 function isInCollection(title) {
 	for (var i = 0; i < collectionArticleList.length; i++) {
-        if (collectionArticleList[i] == title) return true;
+				if (collectionArticleList[i] == title) return true;
 	}
 	return false;
 }
 
 // some links shouldn't create popups
 function isBadLinkForCollection() {
-    var link = pg.current.link.href;
+		var link = pg.current.link.href;
 	if (link.match(/#/)) return true;
 	if (link.match(/redlink=1/)) return true;
 	if (link.match(/action=edit/)) return true;
-    var ns = getNamespaceNumber(pg.current.article.namespace());
-    for (var i = 0; i < wgCollectionArticleNamespaces.length; i++) {
-        if (ns == 14) return false; // NS_CATEGORY
-        if (ns == wgCollectionArticleNamespaces[i]) return false;
-    }
-    return true;
+		var ns = getNamespaceNumber(pg.current.article.namespace());
+		for (var i = 0; i < wgCollectionArticleNamespaces.length; i++) {
+				if (ns == 14) return false; // NS_CATEGORY
+				if (ns == wgCollectionArticleNamespaces[i]) return false;
+		}
+		return true;
 }
 
 function getNamespaceNumber(ns) {
-    return popup_gadget_ns_repair[pg.ns.index[ns]];
+		return popup_gadget_ns_repair[pg.ns.index[ns]];
 }
 
 addOnloadHook(function() {
 	// replace two methods from the Navpopup object
 	Navpopup.prototype.unhide = collectionPopupUnhide;
 	Navpopup.prototype.setInnerHTML = collectionPopupSetInnerHTML;
-    // disable article fetching:
-    pg.option.simplePopups = true;
+		// disable article fetching:
+		pg.option.simplePopups = true;
 	refreshCollectionArticleList();
 });
 
