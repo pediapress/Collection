c4ae24f8ad641b38ce1749d655352aaa7906f8e4
jojo <jojo@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-07 14:30:29 +0000

    system msg adjusted
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/Collection@55941 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Collection.i18n.php
+++ Collection.i18n.php
@@ -85,7 +85,7 @@ This page will not work correctly, unless JavaScript is enabled.</strong>',
 	'coll-personal_collection_label'  => 'Personal book:',
 	'coll-community_collection_label' => 'Community book:',
 	'coll-save_collection'            => 'Save book',
-	'coll-save_category'              => 'All books are saved in the category [[:Category:{{MediaWiki:Coll-bookscategory}}|{{MediaWiki:Coll-bookscategory}}]].',
+	'coll-save_category'              => 'All saved books are added to the category [[:Category:{{MediaWiki:Coll-bookscategory}}|{{MediaWiki:Coll-bookscategory}}]].',
 	'coll-overwrite_title'            => 'Page exists.
 Overwrite?',
 	'coll-overwrite_text'             => 'A page with the name [[:$1]] already exists.
